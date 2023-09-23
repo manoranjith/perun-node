086670f2d8e182c08e5347cd8be09e01563e656f
Manoranjith <ponraj.manoranjitha@in.bosch.com>
2023-09-23 02:04:53 +0200

    Some more testing
    Signed-off-by: Manoranjith <ponraj.manoranjitha@in.bosch.com>

--- api/coap/funding.go
+++ api/coap/funding.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/hyperledger-labs/perun-node/api/grpc/pb"
 	coap "github.com/plgd-dev/go-coap/v3"
 	"github.com/plgd-dev/go-coap/v3/message"
 	"github.com/plgd-dev/go-coap/v3/message/codes"
 	"github.com/plgd-dev/go-coap/v3/mux"
+	"google.golang.org/protobuf/proto"
 )
 
 func loggingMiddleware(next mux.Handler) mux.Handler {
@@ -19,8 +21,53 @@ func loggingMiddleware(next mux.Handler) mux.Handler {
 }
 
 func handleA(w mux.ResponseWriter, r *mux.Message) {
-	fmt.Println("AAA")
-	err := w.SetResponse(codes.Content, message.TextPlain, bytes.NewReader([]byte("hello world")))
+	requestCode := r.Code().String()
+	requestType := r.Type().String()
+	requestToken := r.Token().String()
+	requestMessageID := r.MessageID()
+
+	contentFormat, err := r.ContentFormat()
+	if err != nil {
+		// handler error
+	}
+	payload, err := r.ReadBody()
+	if err != nil {
+		// handler error
+	}
+	protoFundingReq := pb.FundReq{}
+	err = proto.Unmarshal(payload, &protoFundingReq)
+	if err != nil {
+		// handler error
+	}
+	fmt.Println(
+		"g",
+		protoFundingReq.SessionID,
+		protoFundingReq.Params.Id,
+		protoFundingReq.Params.ChallengeDuration,
+		protoFundingReq.Params.Parts,
+		protoFundingReq.Params.App,
+		protoFundingReq.Params.Nonce,
+		protoFundingReq.Params.LedgerChannel,
+		protoFundingReq.Params.VirtualChannel,
+		protoFundingReq.State.Id,
+		protoFundingReq.State.Version,
+		protoFundingReq.State.App,
+		protoFundingReq.State.Allocation,
+		protoFundingReq.State.Data,
+		protoFundingReq.State.IsFinal,
+	)
+	fundingReq, err := pb.ToFundingReq(&protoFundingReq)
+	if err != nil {
+		// handler error
+	}
+	fmt.Println(
+		requestCode,
+		requestType,
+		requestToken,
+		requestMessageID,
+		contentFormat.String(),
+		fundingReq)
+	err = w.SetResponse(codes.Content, message.TextPlain, bytes.NewReader([]byte("hello world")))
 	if err != nil {
 		log.Printf("cannot set response: %v", err)
 	}
@@ -46,6 +93,5 @@ func Serve() {
 	r.Handle("/a", mux.HandlerFunc(handleA))
 	r.Handle("/b", mux.HandlerFunc(handleB))
 
-	fmt.Println("serving")
 	log.Fatal(coap.ListenAndServe("udp", ":5688", r))
 }
--- api/coap/funding_test.go
+++ api/coap/funding_test.go
@@ -1,12 +1,22 @@
 package peruncoap
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 	"log"
 	"testing"
 	"time"
 
+	"github.com/hyperledger-labs/perun-node/api/grpc/pb"
+	"github.com/plgd-dev/go-coap/v3/message"
 	"github.com/plgd-dev/go-coap/v3/udp"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
+	ethchanneltest "perun.network/go-perun/backend/ethereum/channel/test"
+	pchannel "perun.network/go-perun/channel"
+	channeltest "perun.network/go-perun/channel/test"
+	pkgtest "polycry.pt/poly-go/test"
 )
 
 func TestServerResponse(t *testing.T) {
@@ -17,12 +27,36 @@ func TestServerResponse(t *testing.T) {
 		log.Fatalf("Error dialing: %v", err)
 	}
 
-	path := "/a"
-
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	resp, err := co.Get(ctx, path)
+	path := "/a"
+	contentFormat := message.AppOctets
+	// payload := bytes.NewReader([]byte("Hello, World!"))
+	fundingReq := NewRandomFundReq(t)
+	fmt.Println(
+		"k",
+		fundingReq.SessionID,
+		fundingReq.Params.Id,
+		fundingReq.Params.ChallengeDuration,
+		fundingReq.Params.Parts,
+		fundingReq.Params.App,
+		fundingReq.Params.Nonce,
+		fundingReq.Params.LedgerChannel,
+		fundingReq.Params.VirtualChannel,
+		fundingReq.State.Id,
+		fundingReq.State.Version,
+		fundingReq.State.App,
+		fundingReq.State.Allocation,
+		fundingReq.State.Data,
+		fundingReq.State.IsFinal,
+	)
+
+	require.NoError(t, err)
+	payload, err := proto.Marshal(fundingReq)
+	require.NoError(t, err)
+
+	resp, err := co.Post(ctx, path, contentFormat, bytes.NewReader(payload))
 	if err != nil {
 		log.Fatalf("Error sending request: %v", err)
 	}
@@ -35,4 +69,32 @@ func TestServerResponse(t *testing.T) {
 	log.Printf("Response payload code: %s", resp.Code().String())
 	log.Printf("Response payload token: %s", resp.Token())
 	log.Printf("Response payload type: %s", resp.Type().String())
+}
+
+const TxFinalityDepth = 3 // For tests that use a constant finality depth.
+const blockInterval = 50 * time.Millisecond
+
+func NewRandomFundReq(t *testing.T) *pb.FundReq {
+	rng := pkgtest.Prng(t)
+
+	s := ethchanneltest.NewSetup(t, rng, 1, blockInterval, TxFinalityDepth)
+	params, state := channeltest.NewRandomParamsAndState(
+		rng,
+		channeltest.WithChallengeDuration(uint64(100*time.Second)),
+		channeltest.WithParts(s.Parts...),
+		channeltest.WithAssets(s.Asset),
+		channeltest.WithIsFinal(false),
+		channeltest.WithLedgerChannel(true),
+		channeltest.WithVirtualChannel(false),
+	)
+	fundingReq := pchannel.FundingReq{
+		Params:    params,
+		State:     state,
+		Idx:       0,
+		Agreement: channeltest.NewRandomBalances(rng),
+	}
+
+	req, err := pb.FromFundingReq(fundingReq)
+	require.NoError(t, err)
+	return req
 }
