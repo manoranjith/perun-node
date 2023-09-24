157d5f067d1b67a399910ff224ca2750d9d00bc3
Manoranjith <ponraj.manoranjitha@in.bosch.com>
2023-09-25 01:01:33 +0200

    Refactor helper methods in coap funding
    Signed-off-by: Manoranjith <ponraj.manoranjitha@in.bosch.com>

--- api/coap/funding.go
+++ api/coap/funding.go
@@ -1,16 +1,13 @@
 package peruncoap
 
 import (
-	"bytes"
 	"context"
 	"log"
 
 	"github.com/hyperledger-labs/perun-node/api/grpc/pb"
 	"github.com/hyperledger-labs/perun-node/api/handlers"
-	"github.com/plgd-dev/go-coap/v3/message"
 	"github.com/plgd-dev/go-coap/v3/message/codes"
 	"github.com/plgd-dev/go-coap/v3/mux"
-	"google.golang.org/protobuf/proto"
 )
 
 // fundingServer represents a grpc server that can serve funding API.
@@ -27,144 +24,88 @@ func loggingMiddleware(next mux.Handler) mux.Handler {
 
 func (f *fundingServer) Fund(w mux.ResponseWriter, r *mux.Message) {
 	req := pb.FundReq{}
-	if !f.validateAndParseRequest(w, r, &req) {
+	if !validateAndParseRequest(w, r, &req) {
 		return
 	}
 
 	resp, err := f.FundingHandler.Fund(context.TODO(), &req)
 	if err != nil {
-		f.sendErrorResponse(w, codes.InternalServerError, "Cannot parse the response")
+		setErrorResponse(w, codes.InternalServerError, "Cannot parse the response")
 		return
 	}
 
-	f.sendResponse(w, codes.Content, resp)
+	setResponse(w, codes.Content, resp)
 }
 
 func (f *fundingServer) RegisterAssetERC20(w mux.ResponseWriter, r *mux.Message) {
 	req := pb.RegisterAssetERC20Req{}
-	if !f.validateAndParseRequest(w, r, &req) {
+	if !validateAndParseRequest(w, r, &req) {
 		return
 	}
 
 	resp, err := f.FundingHandler.RegisterAssetERC20(context.TODO(), &req)
 	if err != nil {
-		f.sendErrorResponse(w, codes.InternalServerError, "Cannot parse the response")
+		setErrorResponse(w, codes.InternalServerError, "Cannot parse the response")
 		return
 	}
 
-	f.sendResponse(w, codes.Content, resp)
+	setResponse(w, codes.Content, resp)
 }
 func (f *fundingServer) IsAssetRegistered(w mux.ResponseWriter, r *mux.Message) {
 	req := pb.IsAssetRegisteredReq{}
-	if !f.validateAndParseRequest(w, r, &req) {
+	if !validateAndParseRequest(w, r, &req) {
 		return
 	}
 
 	resp, err := f.FundingHandler.IsAssetRegistered(context.TODO(), &req)
 	if err != nil {
-		f.sendErrorResponse(w, codes.InternalServerError, "Cannot parse the response")
+		setErrorResponse(w, codes.InternalServerError, "Cannot parse the response")
 		return
 	}
 
-	f.sendResponse(w, codes.Content, resp)
+	setResponse(w, codes.Content, resp)
 }
 func (f *fundingServer) Register(w mux.ResponseWriter, r *mux.Message) {
 	req := pb.RegisterReq{}
-	if !f.validateAndParseRequest(w, r, &req) {
+	if !validateAndParseRequest(w, r, &req) {
 		return
 	}
 
 	resp, err := f.FundingHandler.Register(context.TODO(), &req)
 	if err != nil {
-		f.sendErrorResponse(w, codes.InternalServerError, "Cannot parse the response")
+		setErrorResponse(w, codes.InternalServerError, "Cannot parse the response")
 		return
 	}
 
-	f.sendResponse(w, codes.Content, resp)
+	setResponse(w, codes.Content, resp)
 }
 
 func (f *fundingServer) Withdraw(w mux.ResponseWriter, r *mux.Message) {
 	req := pb.WithdrawReq{}
-	if !f.validateAndParseRequest(w, r, &req) {
+	if !validateAndParseRequest(w, r, &req) {
 		return
 	}
 
 	resp, err := f.FundingHandler.Withdraw(context.TODO(), &req)
 	if err != nil {
-		f.sendErrorResponse(w, codes.InternalServerError, "Cannot parse the response")
+		setErrorResponse(w, codes.InternalServerError, "Cannot parse the response")
 		return
 	}
 
-	f.sendResponse(w, codes.Content, resp)
+	setResponse(w, codes.Content, resp)
 }
 
 func (f *fundingServer) Progress(w mux.ResponseWriter, r *mux.Message) {
 	req := pb.ProgressReq{}
-	if !f.validateAndParseRequest(w, r, &req) {
+	if !validateAndParseRequest(w, r, &req) {
 		return
 	}
 
 	resp, err := f.FundingHandler.Progress(context.TODO(), &req)
 	if err != nil {
-		f.sendErrorResponse(w, codes.InternalServerError, "Cannot parse the response")
+		setErrorResponse(w, codes.InternalServerError, "Cannot parse the response")
 		return
 	}
 
-	f.sendResponse(w, codes.Content, resp)
-}
-
-func (f *fundingServer) validateAndParseRequest(w mux.ResponseWriter, r *mux.Message, req proto.Message) bool {
-	if r.Code() != codes.POST {
-		f.sendErrorResponse(w, codes.BadOption, "Only POST method is supported")
-		return false
-	}
-
-	if r.Type() != message.Confirmable {
-		f.sendErrorResponse(w, codes.BadOption, "Only Confirmable message type is supported")
-		return false
-	}
-
-	cf, _ := r.ContentFormat()
-	if cf != message.AppOctets {
-		f.sendErrorResponse(w, codes.UnsupportedMediaType, "Only Octet stream media type is supported")
-		return false
-	}
-
-	err := f.parseRequest(r, req)
-	if err != nil {
-		f.sendErrorResponse(w, codes.BadOption, "Cannot parse message body")
-		return false
-	}
-
-	return true
-}
-
-func (f *fundingServer) parseRequest(r *mux.Message, req proto.Message) error {
-	payload, err := r.ReadBody()
-	if err != nil {
-		return err
-	}
-
-	err = proto.Unmarshal(payload, req)
-	return err
-}
-
-func (f *fundingServer) sendErrorResponse(w mux.ResponseWriter, code codes.Code, info string) {
-	err := w.SetResponse(code, message.TextPlain, bytes.NewReader([]byte(info)))
-	if err != nil {
-		log.Printf("cannot set response: %v", err)
-	}
-}
-
-func (f *fundingServer) sendResponse(w mux.ResponseWriter, code codes.Code, resp proto.Message) {
-	payload, err := proto.Marshal(resp)
-	if err != nil {
-		f.sendErrorResponse(w, codes.InternalServerError, "Cannot parse the response")
-		return
-	}
-
-	err = w.SetResponse(code, message.AppOctets, bytes.NewReader(payload))
-	if err != nil {
-		log.Printf("cannot set response: %v", err)
-	}
+	setResponse(w, codes.Content, resp)
 }
